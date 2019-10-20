d2851349a7be9e765e8da6a1432e7b72a9325173
Ivan Shoshnikov <shoshnikov.ivan@mail.ru>
2019-10-21 00:56:49 +0300

    First commit

---
+++ Lab4/Lab4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+int main()
+{
+	string dom;
+	setlocale(LC_ALL, "RUS");
+	cout << "Введите домен\n";
+	cin >> dom;
+		bool flag = true;
+		for (int i = 0; i < dom.length(); i++) 
+			if (dom[i] < 64 || dom[i] > 90 && dom[i] < 97 || dom[i] > 122)
+				flag = false;
+		cout << flag;
+}
