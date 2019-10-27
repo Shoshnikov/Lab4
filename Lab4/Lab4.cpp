30c691e9c31595b1f542aab0c350bf7a84d4ac7e
Ivan Shoshnikov <shoshnikov.ivan@mail.ru>
2019-10-28 01:27:51 +0300

    Correct

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -2,15 +2,48 @@
 #include <locale.h>
 
 using namespace std;
+
+bool addressCheck(string adr, int& zonedom);
+
 int main()
 {
 	string dom;
 	setlocale(LC_ALL, "RUS");
-	cout << "Введите домен\n";
+	cout << "Введите адрес\n";
 	cin >> dom;
+	int zonedom = 0;
 		bool flag = true;
-		for (int i = 0; i < dom.length(); i++) 
-			if (dom[i] < 64 || dom[i] > 90 && dom[i] < 97 || dom[i] > 122)
-				flag = false;
-		cout << flag;
+		if (addressCheck(dom, zonedom))
+		{
+			for (int i = dom.length() - 1; i > dom.length() - zonedom;i--)
+				if (dom[i] < 64 || dom[i] > 90 && dom[i] < 97 || dom[i] > 122)
+					flag = false;
+			cout << flag;
+		}
+		else
+			cout << "Не адрес\n";
+}
+
+bool addressCheck(string adr, int &zonedome)
+{
+	int countBeforeSobaka = 0;
+	int countAfterSobaka = 0;
+	bool sobaka = false;
+	for (int i = 0; i < adr.length(); i++)
+	{
+		if ((int)adr[i] != 64 && sobaka == false)
+			countBeforeSobaka++;
+		else if (sobaka == true)
+			countAfterSobaka++;
+
+		if ((int)adr[i] == 64)
+			sobaka = true;
+	}
+	if (sobaka && countAfterSobaka && countBeforeSobaka)
+		for (int i = adr.length() - 1; (int)adr[i] != 46; i--)
+			zonedome++;
+
+	if (sobaka && countAfterSobaka && countBeforeSobaka)
+		return true;
+	return false;
 }
